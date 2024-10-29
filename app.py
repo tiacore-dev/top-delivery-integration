2599da659c1cbf0b48cd928da395d8a29092c907
Viktoria <v.s.shuvaeva@gmail.com>
2024-10-29 11:14:58 +0700

    fix_auth

--- app.py
+++ app.py
@@ -9,29 +9,35 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-@app.route('/getShipmentsByParams', methods=['GET'])
+@app.route('/getShipmentsByParams', methods=['POST'])
 def get_shipment():
     logger.info('Вызывается getShipmentsByParams')
-    response = td.get_shipments()
+    data = request.json
+    auth_data = data['auth']
+    id = data['receiver_id']
+    response = td.get_shipments(auth_data, id)
     return jsonify(response)
 
 @app.route('/getOrdersByParams', methods=['POST'])
 def get_orders_by_params():
     data = request.json
     shipment_id = data['shipment_id']
-    orders = td.get_orders(shipment_id)
+    auth_data = data['auth']
+    orders = td.get_orders(auth_data, shipment_id)
     return jsonify(orders)
 
 @app.route('/getOrdersInfo', methods = ['POST'])
 def get_order_info():
     data = request.json
     order_id=data['order_id']
-    order_info = td.get_order_info(order_id)
+    auth_data = data['auth']
+    order_info = td.get_order_info(auth_data, order_id)
     return jsonify(order_info)
 
 @app.route('/setOrdersFinalStatus', methods=['POST'])
 def set_orders_final_status():
     data = request.json
+    auth_data = data['auth']
     order_id = data['order_id']
     bar_code = data['bar_code']
     status = data['status']
@@ -41,38 +47,41 @@ def set_orders_final_status():
     payment_type = data.get('payment_type') 
     client_paid = data.get('client_paid')  
 
-    response = td.set_final_status(order_id, bar_code, status, deny_type, date_fact_delivery, payment_type, client_paid)
+    response = td.set_final_status(auth_data, order_id, bar_code, status, deny_type, date_fact_delivery, payment_type, client_paid)
     return jsonify(response)
 
 @app.route('/saveScanningResults', methods=['POST'])
 def save_scanning_results():
     data = request.json
+    auth_data = data['auth']
     shipment_id = data['shipment_id']
     order_ids = [order['orderId'] for order in data['order_ids']]
     
-    response = td.save_scanning_results(shipment_id, order_ids)
+    response = td.save_scanning_results(auth_data, shipment_id, order_ids)
     return jsonify(response)
 
 @app.route('/setOrderProblemStatus', methods = ['POST'])
 def set_order_problem_status():
     data = request.json
+    auth_data = data['auth']
     order_id = data['order_id']
     problem_status = data['problem_status'] # Это json, там два параметра
     note = data['note']
-    response = td.set_problem_status(order_id, problem_status, note)
+    response = td.set_problem_status(auth_data, order_id, problem_status, note)
     return jsonify(response)
 
 
 @app.route('/setOrdersSentToDelivery', methods = ['POST'])
 def set_orders_sent_to_delivery():
     data = request.json
+    auth_data = data['auth']
     order_id = data['order_id'] 
     date_sent_to_delivery = ['date_sent_to_delivery']
     
     delivery_type = data.get('delivery_type')
     service_id = data.get('service_id')
     delivery_params = data.get('delivery_params')
-    response = td.set_sent_to_delivery(order_id, date_sent_to_delivery, delivery_type, service_id, delivery_params)
+    response = td.set_sent_to_delivery(auth_data, order_id, date_sent_to_delivery, delivery_type, service_id, delivery_params)
     return jsonify(response)
 
 
--- td.py
+++ td.py
@@ -17,13 +17,7 @@ logger = logging.getLogger(__name__)
 wsdl_url = os.getenv('WSDL_URL')  # Подставьте сюда актуальный URL
 login_soap = os.getenv('SOAP_LOGIN')
 password_soap = os.getenv('SOAP_PASSWORD')
-user = os.getenv('USER')
-password=os.getenv('PASS')
 
-auth_data = {
-        'login': user,  
-        'password': password   
-    }
 
 # Настроим сессию с аутентификацией
 session = requests.Session()
@@ -31,14 +25,12 @@ session.auth = HTTPBasicAuth(login_soap, password_soap)
 client = zeep.Client(wsdl=wsdl_url, transport=Transport(session=session))
 
 
-
-
 # 2.1 Получить все отправления заказов, которые ушли со склада ТД, но еще не приняты
-def get_shipments():
+def get_shipments(auth_data, id):
     logger.info("Запуск метода get_shipments")
     response = client.service.getShipmentsByParams(
         auth=auth_data,
-        shipmentDirection={'receiverId': 418},
+        shipmentDirection={'receiverId': id},
         shipmentStatus={'id': '3'}
     )
     logger.info(f"Ответ от get_shipments: {response}")
@@ -52,7 +44,7 @@ def get_shipments():
     return shipments
 
 # 2.2 Получить список заказов для каждого отправления
-def get_orders(shipment_id):
+def get_orders(auth_data, shipment_id):
     logger.info(f"Запуск метода get_orders с параметром shipment_id={shipment_id}")
     response = client.service.getOrdersByParams(auth=auth_data, currentShipment=shipment_id)
     logger.info(f"Ответ от get_orders: {response}")
@@ -61,7 +53,7 @@ def get_orders(shipment_id):
     return orders
 
 # Получить детальную информацию по каждому заказу
-def get_order_info(order_id):
+def get_order_info(auth_data, order_id):
     logger.info(f"Запуск метода get_order_info с параметром order_id={order_id}")
     response = client.service.getOrdersInfo(auth=auth_data, order={'orderId': order_id})
     logger.info(f"Ответ от get_order_info: {response}")
@@ -70,7 +62,7 @@ def get_order_info(order_id):
     return order_info
 
 # 2.3 Передать финальный статус заказа в ТД
-def set_final_status(order_id, bar_code, status, deny_type=None, date_fact_delivery=None, payment_type='CARD', client_paid=0):
+def set_final_status(auth_data, order_id, bar_code, status, deny_type=None, date_fact_delivery=None, payment_type='CARD', client_paid=0):
     access_code = hashlib.md5(f"{order_id}+{bar_code}".encode()).hexdigest()
     logger.info(f"Запуск метода set_final_status с параметрами order_id={order_id}, status={status}, deny_type={deny_type}")
     response = client.service.setOrdersFinalStatus(
@@ -87,7 +79,7 @@ def set_final_status(order_id, bar_code, status, deny_type=None, date_fact_deliv
     return serialize_object(response)
 
 # 2.4 Сохранить результат приема на складе
-def save_scanning_results(shipment_id, order_ids):
+def save_scanning_results(auth_data, shipment_id, order_ids):
     logger.info(f"Запуск метода save_scanning_results с параметрами shipment_id={shipment_id}, order_ids={order_ids}")
     
     # Формируем словарь для передачи заказов
@@ -99,7 +91,7 @@ def save_scanning_results(shipment_id, order_ids):
     return serialize_object(response)
 
 # 2.5 Установить статус проблемности заказу
-def set_problem_status(order_id, problem_status, note=""):
+def set_problem_status(auth_data, order_id, problem_status, note=""):
     logger.info(f"Запуск метода set_problem_status с параметрами order_id={order_id}, problem_status={problem_status}, note={note}")
     response = client.service.setOrderProblemStatus(auth=auth_data, orderId=order_id, problemStatus=problem_status, note=note)
     logger.info(f"Ответ от set_problem_status: {response}")
@@ -107,7 +99,7 @@ def set_problem_status(order_id, problem_status, note=""):
 
 
 # 2.6 Установить статус заказа "выдан на доставку"
-def set_sent_to_delivery(order_id, date_sent_to_delivery, delivery_type=None, service_id=None, delivery_params=None):
+def set_sent_to_delivery(auth_data, order_id, date_sent_to_delivery, delivery_type=None, service_id=None, delivery_params=None):
     logger.info(f"Запуск метода set_sent_to_delivery с параметрами order_id={order_id}, date_sent_to_delivery={date_sent_to_delivery}, delivery_type={delivery_type}, service_id={service_id}, delivery_params={delivery_params}")
     
     # Форматируем дату
