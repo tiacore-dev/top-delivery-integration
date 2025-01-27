f6d1b9ec6917cd38f807cd399a2d521c440935be
Viktoria <v.s.shuvaeva@gmail.com>
2025-01-27 14:43:18 +0700

    get order by barcode

--- app.py
+++ app.py
@@ -1,35 +1,42 @@
-from flask import Flask, request, jsonify
-import td
 import logging
 import os
 from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+import td
 
 load_dotenv()
 
 app = Flask(__name__)
 
 # Настройка логирования
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 # Кастомные исключения
+
+
 class DataValidationError(Exception):
     """Ошибка валидации данных"""
     pass
 
+
 class AuthenticationError(Exception):
     """Ошибка авторизации"""
     pass
+
 
 @app.errorhandler(DataValidationError)
 def handle_data_validation_error(e):
     logger.error(f"Ошибка валидации данных: {e}")
     return jsonify({"error": str(e)}), 400
 
+
 @app.errorhandler(AuthenticationError)
 def handle_authentication_error(e):
     logger.error(f"Ошибка авторизации: {e}")
     return jsonify({"error": str(e)}), 401
+
 
 @app.errorhandler(Exception)
 def handle_general_error(e):
@@ -54,6 +61,7 @@ def get_shipment():
         logger.exception("Ошибка при обработке getShipmentsByParams")
         raise e
 
+
 @app.route('/getOrdersByParams', methods=['POST'])
 def get_orders_by_params():
     try:
@@ -69,7 +77,8 @@ def get_orders_by_params():
     except Exception as e:
         logger.exception("Ошибка при обработке getOrdersByParams")
         raise e
-    
+
+
 @app.route('/getOrdersInfo', methods=['POST'])
 def get_order_info():
     try:
@@ -85,6 +94,7 @@ def get_order_info():
     except Exception as e:
         logger.exception("Ошибка при обработке getOrdersInfo")
         raise e
+
 
 @app.route('/getOrdersInfoWebshop', methods=['POST'])
 def get_order_info_by_webshop():
@@ -102,6 +112,24 @@ def get_order_info_by_webshop():
         logger.exception("Ошибка при обработке getOrdersInfoWebshop")
         raise e
 
+
+@app.route('/getOrdersInfoBarcode', methods=['POST'])
+def get_order_info_by_barcode():
+    try:
+        data = request.json
+        if not data or 'auth' not in data or 'barcode' not in data:
+            raise DataValidationError("auth и barcode обязательны")
+        barcode = data['barcode']
+        auth_data = data['auth']
+        order_info = td.get_order_info_by_barcode(auth_data, barcode)
+        return jsonify(order_info)
+    except DataValidationError as e:
+        raise e
+    except Exception as e:
+        logger.exception("Ошибка при обработке getOrdersInfoWebshop")
+        raise e
+
+
 @app.route('/setOrdersFinalStatus', methods=['POST'])
 def set_orders_final_status():
     try:
@@ -110,13 +138,20 @@ def set_orders_final_status():
             raise DataValidationError("auth и webshop_number обязательны")
         auth_data = data['auth']
         work_status = data.get('work_status')
-        webshop_number = data['webshop_number']
+        webshop_number = data.get('webshop_number', None)
+        order_id = data.get('order_id', None)
+        barcode = data.get('barcode', None)
         deny_params = data.get('deny_params', None)
         date_fact_delivery = data.get('date_fact_delivery')
-        payment_type = data.get('payment_type') 
-        client_paid = data.get('client_paid')  
+        payment_type = data.get('payment_type')
+        client_paid = data.get('client_paid')
         delivery_paid = data.get('delivery_paid')
-        response = td.set_final_status(auth_data, webshop_number, date_fact_delivery, client_paid, work_status, delivery_paid, payment_type, deny_params)
+
+        if not (order_id or barcode or webshop_number):
+            return jsonify({"msg": "Error: no data"}), 400
+
+        response = td.set_final_status(auth_data, order_id, barcode, webshop_number, date_fact_delivery,
+                                       client_paid, work_status, delivery_paid, payment_type, deny_params)
         return jsonify(response)
     except DataValidationError as e:
         raise e
@@ -124,12 +159,14 @@ def set_orders_final_status():
         logger.exception("Ошибка при обработке setOrdersFinalStatus")
         raise e
 
+
 @app.route('/saveScanningResults', methods=['POST'])
 def save_scanning_results():
     try:
         data = request.json
         if not data or 'auth' not in data or 'shipment_id' not in data or 'order_ids' not in data:
-            raise DataValidationError("auth, shipment_id и order_ids обязательны")
+            raise DataValidationError(
+                "auth, shipment_id и order_ids обязательны")
         auth_data = data['auth']
         shipment_id = data['shipment_id']
         order_ids = data['order_ids']
@@ -141,17 +178,20 @@ def save_scanning_results():
         logger.exception("Ошибка при обработке saveScanningResults")
         raise e
 
+
 @app.route('/setOrderProblemStatus', methods=['POST'])
 def set_order_problem_status():
     try:
         data = request.json
         if not data or 'auth' not in data or 'webshop_number' not in data or 'problem_status' not in data:
-            raise DataValidationError("auth, webshop_number и problem_status обязательны")
+            raise DataValidationError(
+                "auth, webshop_number и problem_status обязательны")
         auth_data = data['auth']
         webshop_number = data['webshop_number']
         problem_status = data['problem_status']  # Это json, там два параметра
         note = data.get('note')
-        response = td.set_problem_status(auth_data, webshop_number, problem_status, note)
+        response = td.set_problem_status(
+            auth_data, webshop_number, problem_status, note)
         return jsonify(response)
     except DataValidationError as e:
         raise e
@@ -159,19 +199,22 @@ def set_order_problem_status():
         logger.exception("Ошибка при обработке setOrderProblemStatus")
         raise e
 
+
 @app.route('/setOrdersSentToDelivery', methods=['POST'])
 def set_orders_sent_to_delivery():
     try:
         data = request.json
         if not data or 'auth' not in data or 'order_id' not in data or 'date_sent_to_delivery' not in data:
-            raise DataValidationError("auth, order_id и date_sent_to_delivery обязательны")
+            raise DataValidationError(
+                "auth, order_id и date_sent_to_delivery обязательны")
         auth_data = data['auth']
-        order_id = data['order_id'] 
+        order_id = data['order_id']
         date_sent_to_delivery = data['date_sent_to_delivery']
         delivery_type = data.get('delivery_type')
         service_id = data.get('service_id')
         delivery_params = data.get('delivery_params')
-        response = td.set_sent_to_delivery(auth_data, order_id, date_sent_to_delivery, delivery_type, service_id, delivery_params)
+        response = td.set_sent_to_delivery(
+            auth_data, order_id, date_sent_to_delivery, delivery_type, service_id, delivery_params)
         return jsonify(response)
     except DataValidationError as e:
         raise e
--- td.py
+++ td.py
@@ -1,12 +1,12 @@
+import os
+import logging
+import hashlib
+from zeep.helpers import serialize_object
 import zeep
 import requests
 from zeep.transports import Transport
 from requests.auth import HTTPBasicAuth
-import hashlib
-from zeep.helpers import serialize_object
-import os
 from dotenv import load_dotenv
-import logging
 from utils import serialize_dates
 
 load_dotenv()
@@ -24,6 +24,8 @@ session.auth = HTTPBasicAuth(login_soap, password_soap)
 client = zeep.Client(wsdl=wsdl_url, transport=Transport(session=session))
 
 # 2.1 Получить все отправления заказов, которые ушли со склада ТД, но еще не приняты
+
+
 def get_shipments(auth_data, id):
     try:
         logger.info("Запуск метода get_shipments")
@@ -41,10 +43,14 @@ def get_shipments(auth_data, id):
         return None, 500
 
 # 2.2 Получить список заказов для каждого отправления
+
+
 def get_orders(auth_data, shipment_id):
     try:
-        logger.info(f"Запуск метода get_orders с параметром shipment_id={shipment_id}")
-        response = client.service.getOrdersByParams(auth=auth_data, currentShipment=shipment_id)
+        logger.info(
+            f"Запуск метода get_orders с параметром shipment_id={shipment_id}")
+        response = client.service.getOrdersByParams(
+            auth=auth_data, currentShipment=shipment_id)
         logger.info(f"Ответ от get_orders: {response}")
         orders = serialize_object(response)
         orders = serialize_dates(orders)
@@ -54,10 +60,14 @@ def get_orders(auth_data, shipment_id):
         return None, 500
 
 # Получить детальную информацию по каждому заказу
+
+
 def get_order_info(auth_data, order_id):
     try:
-        logger.info(f"Запуск метода get_order_info с параметром order_id={order_id}")
-        response = client.service.getOrdersInfo(auth=auth_data, order={'orderId': order_id})
+        logger.info(
+            f"Запуск метода get_order_info с параметром order_id={order_id}")
+        response = client.service.getOrdersInfo(
+            auth=auth_data, order={'orderId': order_id})
         logger.info(f"Ответ от get_order_info: {response}")
         order_info = serialize_object(response)
         order_info = serialize_dates(order_info)
@@ -66,32 +76,40 @@ def get_order_info(auth_data, order_id):
         logger.error(f"Ошибка в get_order_info: {e}", exc_info=True)
         return None
 
-def get_order_id(auth_data, webshop_number):
+
+def get_order_id(auth_data, webshop_number, barcode):
     try:
-        data = get_order_info_by_webshop(auth_data, webshop_number)
+        if webshop_number:
+            data = get_order_info_by_webshop(auth_data, webshop_number)
+        else:
+            data = get_order_info_by_barcode(auth_data, barcode)
         if not data or 'orderInfo' not in data:
             raise ValueError("Данные orderInfo отсутствуют в ответе")
-        
+
         order_info = data.get("orderInfo", [])[0]
         order_identity = order_info.get("orderIdentity")
-        
+
         if not order_identity:
             raise ValueError("Данные orderIdentity отсутствуют в orderInfo")
-        
+
         # Исправлено: правильное имя ключа
-        order_id = order_identity['orderId']  
+        order_id = order_identity['orderId']
         bar_code = order_identity['barcode']
-        
-        return order_id, bar_code
+        webshop_number = order_identity['webshopNumber']
+
+        return order_id, bar_code, webshop_number
     except Exception as e:
         logger.error(f"Ошибка в get_order_id: {e}", exc_info=True)
-        return None, None
+        return None, None, None
+
 
 def get_order_info_by_webshop(auth_data, webshop_number):
     try:
-        logger.info(f"Запуск метода get_order_info_by_webshop с параметром webshop_number={webshop_number}")
+        logger.info(f"""Запуск метода get_order_info_by_webshop с параметром webshop_number={
+                    webshop_number}""")
         order = {"webshopNumber": webshop_number}
-        response = client.service.getOrdersByParams(auth=auth_data, orderIdentity=order)
+        response = client.service.getOrdersByParams(
+            auth=auth_data, orderIdentity=order)
         logger.info(f"Ответ от get_order_info_by_webshop: {response}")
         order_info = serialize_object(response)
         order_info = serialize_dates(order_info)
@@ -100,14 +118,42 @@ def get_order_info_by_webshop(auth_data, webshop_number):
         logger.error(f"Ошибка в get_order_info_by_webshop: {e}", exc_info=True)
         return None
 
-# 2.3 Передать финальный статус заказа в ТД
-def set_final_status(auth_data, webshop_number, date_fact_delivery, client_paid, work_status, delivery_paid, payment_type, deny_params=None):
+
+def get_order_info_by_barcode(auth_data, barcode):
     try:
-        order_id, bar_code = get_order_id(auth_data, webshop_number)
-        if not order_id or not bar_code:
+        logger.info(f"""Запуск метода get_order_info_by_barcode с параметром barcode={
+                    barcode}""")
+        order = {"barcode": barcode}
+        response = client.service.getOrdersByParams(
+            auth=auth_data, orderIdentity=order)
+        logger.info(f"Ответ от get_order_info_by_barcode: {response}")
+        order_info = serialize_object(response)
+        order_info = serialize_dates(order_info)
+        return order_info
+    except Exception as e:
+        logger.error(f"Ошибка в get_order_info_by_barcode: {e}", exc_info=True)
+        return None
+
+# 2.3 Передать финальный статус заказа в ТД
+
+
+def set_final_status(auth_data, order_id, barcode, webshop_number, date_fact_delivery, client_paid, work_status, delivery_paid, payment_type, deny_params=None):
+    try:
+        if order_id:
+            data = get_order_info(auth_data, order_id)
+            order_info = data.get("orderInfo", [])[0]
+            order_identity = order_info.get("orderIdentity")
+            bar_code = order_identity['barcode']
+            webshop_number = order_identity['webshopNumber']
+        else:
+            order_id, bar_code, webshop_number = get_order_id(
+                auth_data, webshop_number, barcode)
+        if not order_id:
             raise ValueError("Не удалось получить order_id или barcode")
-        access_code = hashlib.md5(f"{str(order_id)}+{bar_code}".encode()).hexdigest()
-        logger.info(f"Запуск метода set_final_status с параметрами order_id={order_id}, workStatus={work_status},")
+        access_code = hashlib.md5(
+            f"{str(order_id)}+{bar_code}".encode()).hexdigest()
+        logger.info(f"""Запуск метода set_final_status с параметрами order_id={
+                    order_id}, workStatus={work_status},""")
         final_status_params = {
             "orderIdentity": {
                 "orderId": order_id,
@@ -124,7 +170,7 @@ def set_final_status(auth_data, webshop_number, date_fact_delivery, client_paid,
         if work_status['id'] == "5" and deny_params:
             final_status_params["denyParams"] = deny_params
         response = client.service.setOrdersFinalStatus(
-            auth=auth_data, 
+            auth=auth_data,
             finalStatusParams=[final_status_params]
         )
         logger.info(f"Ответ от set_final_status: {response}")
@@ -134,30 +180,37 @@ def set_final_status(auth_data, webshop_number, date_fact_delivery, client_paid,
         return None, 500
 
 # 2.4 Сохранить результат приема на складе
+
+
 def save_scanning_results(auth_data, shipment_id, orders):
     try:
-        logger.info(f"Запуск метода save_scanning_results с параметрами shipment_id={shipment_id}, orders={orders}")
-        response = client.service.saveScanningResults(auth=auth_data, shipmentId=shipment_id, orders=orders)
+        logger.info(f"""Запуск метода save_scanning_results с параметрами shipment_id={
+                    shipment_id}, orders={orders}""")
+        response = client.service.saveScanningResults(
+            auth=auth_data, shipmentId=shipment_id, orders=orders)
         logger.info(f"Ответ от save_scanning_results: {response}")
         return serialize_object(response)
     except Exception as e:
         logger.error(f"Ошибка в save_scanning_results: {e}", exc_info=True)
         return None
 
+
 def set_problem_status(auth_data, webshop_number, problem_status, note=""):
     try:
         # Получение order_id
-        order_id = get_order_id(auth_data, webshop_number)
+        order_id = get_order_id(auth_data, webshop_number, barcode=None)
         if not order_id:
-            raise ValueError(f"Заказ с webshop_number={webshop_number} не найден")
+            raise ValueError(f"""Заказ с webshop_number={
+                             webshop_number} не найден""")
 
         # Логирование перед вызовом API
-        logger.info(f"Запуск метода set_problem_status с параметрами order_id={order_id}, problem_status={problem_status}, note={note}")
+        logger.info(f"""Запуск метода set_problem_status с параметрами order_id={
+                    order_id}, problem_status={problem_status}, note={note}""")
 
         # Вызов API
         response = client.service.setOrderProblemStatus(
             auth=auth_data,
-            #orderId=order_id,
+            # orderId=order_id,
             problemOrders={"orderIdentity": order_id},
             problemStatus=problem_status,
             note=note
@@ -168,7 +221,8 @@ def set_problem_status(auth_data, webshop_number, problem_status, note=""):
 
         # Проверка на ошибки в ответе
         if not response or response.get('status') != 'success':
-            raise RuntimeError(f"Ошибка при установке статуса проблемности: {response}")
+            raise RuntimeError(
+                f"Ошибка при установке статуса проблемности: {response}")
 
         # Возврат успешного результата
         return serialize_object(response), 200
@@ -184,7 +238,8 @@ def set_problem_status(auth_data, webshop_number, problem_status, note=""):
 # 2.6 Установить статус заказа "выдан на доставку"
 def set_sent_to_delivery(auth_data, order_id, date_sent_to_delivery, delivery_type=None, service_id=None, delivery_params=None):
     try:
-        logger.info(f"Запуск метода set_sent_to_delivery с параметрами order_id={order_id}, date_sent_to_delivery={date_sent_to_delivery}, delivery_type={delivery_type}, service_id={service_id}, delivery_params={delivery_params}")
+        logger.info(f"""Запуск метода set_sent_to_delivery с параметрами order_id={order_id}, date_sent_to_delivery={
+                    date_sent_to_delivery}, delivery_type={delivery_type}, service_id={service_id}, delivery_params={delivery_params}""")
         date_sent = date_sent_to_delivery.strftime('%Y-%m-%d %H:%M:%S')
         order_identity = {'orderId': order_id}
         response = client.service.setOrdersSentToDelivery(
